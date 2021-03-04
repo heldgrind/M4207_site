cd3fc184ac154ac2dd0a7d894eeebc3efea5f92c
heldgrind <ptitcorentin@live.fr>
2021-03-04 10:24:54 +0100

    changement 04/03

--- src/Controller/MonController.php
+++ src/Controller/MonController.php
@@ -5,23 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 class MonController extends AbstractController
 {
     /**
      * @Route("/mon", name="mon")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager): Response
     {
+        $Login= $request->request->get('nom'); // a faire dans route /login qui sera envoyé avec submit
+        $Pass= $request->request->get("password");
+
+        $utilisateur = $manager ->getRepository(Utilisateurs::class)->findOneBy
+
         return $this->render('mon/index.html.twig', [
-            'controller_name' => 'MonController',
+            'controller_name' => 'MonController','login' => $Login
         ]);
     }
     /**
      * @Route("/utilisateurs", name="utilisateurs")
      */
-    public function utilisateurs(): Response
+    public function utilisateurs(Request $request, EntityManagerInterface $manager): Response
     {
+
         return $this->render('mon/utilisateurs.html.twig', [
             'controller_name' => 'MonController',
         ]);
