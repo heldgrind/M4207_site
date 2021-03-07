64a5a2ae6a2c535859f7ebcba64dd046485b1578
heldgrind <ptitcorentin@live.fr>
2021-03-07 20:00:11 +0100

    changement 07/03

--- src/Controller/MonController.php
+++ src/Controller/MonController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Utilisateur;
+use App\Entity\Utilisateurs;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 class MonController extends AbstractController
@@ -15,13 +15,9 @@ class MonController extends AbstractController
      */
     public function index(Request $request, EntityManagerInterface $manager): Response
     {
-        $Login= $request->request->get('nom'); // a faire dans route /login qui sera envoyé avec submit
-        $Pass= $request->request->get("password");
-
-        $utilisateur = $manager ->getRepository(Utilisateurs::class)->findOneBy
 
         return $this->render('mon/index.html.twig', [
-            'controller_name' => 'MonController','login' => $Login
+            'controller_name' => 'MonController',
         ]);
     }
     /**
@@ -32,6 +28,21 @@ class MonController extends AbstractController
 
         return $this->render('mon/utilisateurs.html.twig', [
             'controller_name' => 'MonController',
+        ]);
+    }
+     /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, EntityManagerInterface $manager): Response
+    {
+        $Login= $request->request->get('nom'); // a faire dans route /login qui sera envoyé avec submit
+        $Pass= $request->request->get('password');
+
+        $utilisateur = $manager ->getRepository(utilisateurs::class)->findOneBy(['email' => 'txt']);
+
+        return $this->render('mon/login_result.html.twig', [
+            'controller_name' => 'MonController',
+            'login' => $Login
         ]);
     }
 }
