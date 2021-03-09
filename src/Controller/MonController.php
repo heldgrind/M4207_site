f3672400fe368fe191a2c04b8b1eb1aab832d530
heldgrind <ptitcorentin@live.fr>
2021-03-09 17:29:01 +0100

    ajour creation utilisateur

--- src/Controller/MonController.php
+++ src/Controller/MonController.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Utilisateurs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Utilisateurs;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 class MonController extends AbstractController
@@ -16,12 +15,27 @@ class MonController extends AbstractController
     public function index(Request $request, EntityManagerInterface $manager): Response
     {
 
-        return $this->render('mon/index.html.twig', [
-            'controller_name' => 'MonController',
-        ]);
+        $Login= $request->request->get('email');
+        $Pass= $request->request->get('password');
+
+        $utilisateur = $manager ->getRepository(utilisateurs::class)->findOneBy(array('email'=> $Login));
+
+
+        if ($utilisateur!= NUll){
+            return $this->render('mon/login_result.html.twig', [
+                'controller_name' => 'connexion réussi',
+                'login' => $Login,
+            ]);
+        }
+        else{
+            return $this->render('mon/index.html.twig', [
+                'controller_name' => 'echec de connexion',
+            ]);
+        }
+        
     }
     /**
-     * @Route("/utilisateurs", name="utilisateurs")
+     * @Route("/creation", name="creation")
      */
     public function utilisateurs(Request $request, EntityManagerInterface $manager): Response
     {
@@ -35,14 +49,55 @@ class MonController extends AbstractController
      */
     public function login(Request $request, EntityManagerInterface $manager): Response
     {
-        $Login= $request->request->get('nom'); // a faire dans route /login qui sera envoyé avec submit
-        $Pass= $request->request->get('password');
+        
 
-        $utilisateur = $manager ->getRepository(utilisateurs::class)->findOneBy(['email' => 'txt']);
-
-        return $this->render('mon/login_result.html.twig', [
-            'controller_name' => 'MonController',
-            'login' => $Login
-        ]);
+            return $this->render('mon/login_result.html.twig', [
+                'controller_name' => 'MonController',
+            ]);
+                
+            
+        
+        
     }
+    /**
+     * @Route("/cUser", name="cUser")
+     */
+    public function creation(Request $request, EntityManagerInterface $manager): Response
+    {
+        $recupNom=$request->request->get('nom');
+        $recupPrenom=$request->request->get('prenom');
+        $recupEmail=$request->request->get('email2');
+        $recupPassword=$request->request->get('password');
+        
+        $User = new Utilisateurs();
+		$User->setNom($recupNom);
+		$User->setPrenom($recupPrenom);
+        $User->setEmail($recupEmail);
+        $User->setPassword($recupPassword);
+        $manager->persist($User);
+		$manager->flush();
+
+            
+        return $this->redirectToRoute('listeUser');
+                
+            
+        
+        
+    }
+    /**
+     * @Route("/listeUser", name="listeUser")
+     */
+    public function liste(Request $request, EntityManagerInterface $manager): Response
+    {
+            $listUser=$manager->getRepository(Utilisateurs::class)->findAll();
+
+            return $this->render('mon/listeUser.html.twig', [
+                'listUser' => $listUser
+            ]);
+                
+            
+        
+        
+    }
+
 }
--- src/Entity/Authorisation.php
+++ src/Entity/Authorisation.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=AuthorisationRepository::class)
  */
-class Authorisation
+class authorisation
 {
     /**
      * @ORM\Id
